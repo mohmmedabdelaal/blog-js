9f627465bbdcf1606a7294b8308bfcb136e2e2ce
Moelaal <medo.elaal@gmail.com>
2022-08-31 05:07:49 +0200

    initial setup

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -1,1 +1,12 @@
 import '../assets/css/style.css';
+
+const API_URL = 'http://localhost:3000/api/posts';
+const API_BASE_URL = 'http://localhost:3000/';
+
+window.onload = () => {
+  getPosts();
+};
+
+const getPosts = () => {};
+
+const buildPosts = (blogPosts) => {};
